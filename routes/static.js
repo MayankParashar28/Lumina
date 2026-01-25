d4e18938c01b4f8d49d4112a8a03b33e2ac4cb19
MayankParashar28 <mayankparashar28@gmail.com>
2026-01-25 23:59:31 +0530

    feat: add google analytics, sitemap.xml and update robots.txt

--- routes/static.js
+++ routes/static.js
@@ -1,5 +1,56 @@
 const { Router } = require("express");
+const Blog = require("../models/blog");
 const router = Router();
+
+// Sitemap.xml Generation
+router.get("/sitemap.xml", async (req, res) => {
+    try {
+        const blogs = await Blog.find({ status: "published" }).select("_id updatedAt");
+        const baseUrl = "https://lumina.blog";
+
+        let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>${baseUrl}/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/about</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/privacy</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.3</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/terms</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.3</priority>
+    </url>`;
+
+        blogs.forEach(blog => {
+            sitemap += `
+    <url>
+        <loc>${baseUrl}/blog/${blog._id}</loc>
+        <lastmod>${new Date(blog.updatedAt).toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>`;
+        });
+
+        sitemap += `
+</urlset>`;
+
+        res.header("Content-Type", "application/xml");
+        res.send(sitemap);
+    } catch (err) {
+        console.error("Sitemap Generation Error:", err);
+        res.status(500).end();
+    }
+});
 
 router.get("/about", (req, res) => {
     res.render("about", {
